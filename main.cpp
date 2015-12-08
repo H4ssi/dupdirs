0dd2ff60f84031d505a2a0a042d06122bb529a38
Florian Hassanen <florian.hassanen@gmail.com>
2015-12-08 23:54:02 +0100

    parse command line args for dirs

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@
 #include <vector>
 #include <algorithm>
 
+#include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 
-using namespace boost::filesystem;
 using std::begin;
 using std::end;
+
+namespace po = boost::program_options;
+using namespace boost::filesystem;
 
 template<>
 class std::hash<path> : private std::hash<std::string> {
@@ -36,8 +39,35 @@ void read_dir(path dir) {
     std::sort(begin(directories), end(directories));
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    read_dir("E:\\b");
+    po::options_description desc("Options");
+    desc.add_options()
+        ("help,h", "show this message")
+        ("directory,d", po::value<std::vector<std::string>>()->required(), "directories to be searched")
+        ;
+    po::positional_options_description posd;
+    posd.add("directory", -1);
+
+    po::variables_map vars;
+    try {
+        po::store(po::command_line_parser(argc, argv).options(desc).positional(posd).run(), vars);
+        po::notify(vars);
+    }
+    catch (po::error const & e) {
+        std::cout << desc << std::endl;
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 2;
+    }
+
+    if (vars.count("help")) {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    for (auto dir : vars["directory"].as<std::vector<std::string>>()) {
+        read_dir(dir);
+    }
+
     return 0;
 }
